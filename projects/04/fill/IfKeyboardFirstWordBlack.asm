c54dce5beddf7ca71b061f5bb255429161fa85ef
Winry <winry.tian@gmail.com>
2022-01-09 15:31:53 -0800

    add IfKeyboardFirstWordBlack implementation

--- projects/04/fill/IfKeyboardFirstWordBlack.asm
+++ projects/04/fill/IfKeyboardFirstWordBlack.asm
@@ -10,3 +10,26 @@
 //   (OFF)
 //     set first word to 0 // white
 //     goto line 0 // repeat
+
+
+// decide between on and off
+@KBD
+D=M
+@ON
+D;JNE // goto ON if D != 0
+@OFF
+D;JEQ // goto OFF if D == 0
+
+// if a key is pushed, goto ON
+(ON)
+  @SCREEN
+  M=-1
+  @0
+  0;JMP
+
+// otherwise, goto OFF
+(OFF)
+  @SCREEN
+  M=0
+  @0
+  0;JMP
