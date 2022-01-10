60ab1ac31c9766d193d4aff22710f651f60b94a2
Winry <winry.tian@gmail.com>
2022-01-10 08:36:38 -0800

    add EntireScreenBlack implementation

--- projects/04/fill/EntireScreenBlack.asm
+++ projects/04/fill/EntireScreenBlack.asm
@@ -8,3 +8,29 @@
 //     
 //   END:
 //     goto line 0
+
+
+@i
+M=0
+
+(LOOP)
+  @8192    // length of builtin SCREEN chip
+  D=A
+  @i
+  D=D-M    // D=8192-i
+  @END
+  D;JLT
+
+  @i
+  D=M      // where are we?
+  @SCREEN
+  A=A+D
+  M=-1
+  @i
+  M=M+1
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
